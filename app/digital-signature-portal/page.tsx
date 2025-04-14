716e2f89b68f7e8ad28da2c8f480e4ae116fbd8e
penjorkinley77@gmail.com <you@example.com>
2025-04-14 10:53:48 +0600

    reduced the padding in the main page

--- app/digital-signature-portal/page.tsx
+++ app/digital-signature-portal/page.tsx
@@ -2,7 +2,7 @@ import SignatureForm from "@/components/signature/SignatureForm";
 
 export default function DigitalSignaturePage() {
   return (
-    <main className="flex flex-col items-center pt-16 sm:pt-20 md:pt-24 px-4 pb-12">
+    <main className="flex flex-col items-center pt-12 sm:pt-16 md:pt-16 px-4 pb-12">
       <div className="w-full max-w-3xl mx-auto">
         <SignatureForm />
       </div>
